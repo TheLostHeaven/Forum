d0a8364d978060078c5ae9390a263a8e0b759f07
Daniel Molina <dannyking07@outlook.es>
2024-08-14 01:43:24 -0500

    feat(app.module) config modules

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,17 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { FeaturesModule } from './features/features.module';
+import { SharedModule } from './shared/shared.module';
 @NgModule({
   declarations: [
     AppComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FeaturesModule,
+    SharedModule
   ],
   providers: [],
   bootstrap: [AppComponent]
