cd7f4cde9175edf695fec6d531c94d11e954f57e
Daniel Molina <dannyking07@outlook.es>
2024-08-14 01:42:54 -0500

    feat(app.routing): Add routers components

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,20 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './features/home/home/home.component';
+import { LoginComponent } from './features/auth/login/login.component';
+import { RegisterComponent } from './features/auth/register/register.component';
+import { DashboardComponent } from './features/admin/dashboard/dashboard.component';
+import { PageNotFoundComponent } from './shared/components/page-not-found/page-not-found.component';
+import { ProfileComponent } from './features/home/profile/profile.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  { path: 'register', component: RegisterComponent },
+  { path: 'login', component: LoginComponent },
+  { path: 'profile', component: ProfileComponent},
+  { path: 'dashboard', component: DashboardComponent},
+  { path: '**', component: PageNotFoundComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
